d32d04baefd532c8d273ab3279e610bb22e37378
SerhiiRyzhkov <had0uken@ukr.net>
2022-09-18 22:04:48 +0300

    second commit

--- src/spring_introduction/train/Person.java
+++
@@ -1,6 +0,0 @@
-package spring_introduction.train;
-
-public interface Person {
-    public void introduce();
-    public Pet getPet();
-}
--- src/spring_introduction/train/Pet.java
+++
@@ -1,5 +0,0 @@
-package spring_introduction.train;
-
-public interface Pet {
-    public void say();
-}
